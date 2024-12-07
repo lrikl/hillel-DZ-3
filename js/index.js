91b13ebe1b89b65771b374e1e346d899a6999812
lrikl <92387315+lrikl@users.noreply.github.com>
2024-12-07 19:13:05 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@
 const fiveDigitNumber = 10369;
 
 const result = fiveDigitNumber.toString().split("").join(" "); // перетворили число до рядка -> розділили створений рядок по цифрам (.split розбив рядок на масив цифр-літер "") -> додали пробіл між ними (.join зшив цей масив знову у рядок з пробілом " ")
-console.log(result)
+console.log(result);
 
